1e754b5b1037171d2d32b814988e3346d53441c0
Guilherme Rosmaninho <gui.rosmaninho@gmail.com>
2025-05-18 17:43:07 +0100

    Cores

--- powerudp/server.c
+++ powerudp/server.c
@@ -11,8 +11,17 @@
 #include <pthread.h>
 #include <time.h>
 
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+#define BOLD    "\x1b[1m"
+
 #define MAX_CLIENTS 10
-#define SERVER_LOG_PREFIX "[Servidor] "
+#define SERVER_LOG_PREFIX "\x1b[34m[Servidor]\x1b[0m "
 
 // Structure to store information about a registered client
 typedef struct
